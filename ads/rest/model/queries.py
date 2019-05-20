2df0357e57beec6192a092a05d15569b23637840
unknown <tazdik.ershad2@arbetsformedlingen.se>
2019-05-20 17:26:41 +0200

    adding limit query in  the skills api

--- ads/rest/model/queries.py
+++ ads/rest/model/queries.py
@@ -17,7 +17,7 @@ skillsandtraits_query = reqparse.RequestParser()
 skillsandtraits_query.add_argument('q', required=True,
                                    location='args',
                                    help="Must provide an occupation name!")
-
 skillsandtraits_query.add_argument(settings.LIMIT,
                             type=inputs.int_range(0, settings.MAX_LIMIT))
+skillsandtraits_query.add_argument(settings.SHOW_EXPIRED, choices=['true', 'false'], default='false')
 skillsandtraits_query.add_argument(settings.PLACE, action='append')
