c59b64062ee9baf33a24f3299c02bb16c9160071
unknown <tazdik.ershad2@arbetsformedlingen.se>
2019-05-24 16:25:05 +0200

    job count api

--- ads/apiresources/geokomp.py
+++ ads/apiresources/geokomp.py
@@ -24,19 +24,23 @@ def __setCoordinate(aObject):
     coordinates = {'lat': aObject['koordinater']['WGS84']['x'], 'lng': aObject['koordinater']['WGS84']['y']}
     return coordinates
 
+
+def makeGeoLocationReq(location, reqSession):
+    geokomp_URL = "http://www.arbetsformedlingen.se:80/rest/geo/rest"
+    friText_path = '/v1/fritext'
+    params = {'text': location, 'hints': 'postnummer,gata'}
+    headers = {'Accept': 'application/json', 'applicationId': 'Jobscanner'}
+    response = reqSession.get(geokomp_URL + friText_path, params=params, headers=headers)
+    response.encoding = 'ISO-8859-1'
+    return json.loads(response.text)
+
+
 def getGeoLocation(location, reqSession):
     if not location:
         return {'lat': None, 'lng': None}
     #TODO: move all config to settings.py
     if location not in GeoKomp.tmpCache:
-        geokomp_URL = "http://www.arbetsformedlingen.se:80/rest/geo/rest"
-        friText_path = '/v1/fritext'
-        params = {'text': location, 'hints': 'postnummer,gata'}
-        url = geokomp_URL + friText_path
-        headers = {'Accept': 'application/json', 'applicationId': 'Jobscanner'}
-        response = reqSession.get(geokomp_URL + friText_path, params=params, headers=headers)
-        response.encoding='ISO-8859-1'
-        jresp = json.loads(response.text)
+        jresp = makeGeoLocationReq(location, reqSession)
         if jresp:
             coordinates = __getCoordinates(jresp)
         else:
